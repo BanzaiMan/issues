b4116b792d285beacf1649c8ce21bd8c205657cb
Hiro Asari <asari.ruby@gmail.com>
2015-02-22 23:18:02 -0500

    Add basic parse_args tests

--- test/issues_test.exs
+++ test/issues_test.exs
@@ -1,7 +1,17 @@
 defmodule IssuesTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  import Issues.CLI, only: [ parse_args: 1 ]
+  test ":help returned by option parsing with -h and --help options" do
+    assert parse_args(["-h",     "anything"]) == :help
+    assert parse_args(["--help", "anything"]) == :help
+  end
+
+  test "thre values returned if three are given" do
+    assert parse_args(["user", "project", "99"]) == { "user", "project", 99 }
+  end
+
+  test "count is defaulted if two values are given" do
+    assert parse_args(["user", "project"]) == { "user", "project", Issues.CLI.get_default_count }
   end
 end
