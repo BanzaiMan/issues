e9b6ae001db9a5cea701940ceb1678ffd8296b9c
Hiro Asari <asari.ruby@gmail.com>
2015-02-23 00:11:54 -0500

    Return only a few results

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -46,6 +46,8 @@ defmodule Issues.CLI do
     |> decode_response
     |> convert_to_list_of_hashdicts
     |> sort_into_ascending_order
+    |> Enum.take(_count)
+    |> print_table_for_columns(["number", "created_at", "title"])
   end
 
   def decode_response({:ok, body}) do
